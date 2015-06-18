8e5de640a538e0e98acbd73fd27175f918d44211
Steve Hudson <strayhud@gmail.com>
2015-06-18 16:36:35 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
 # Script to backup and link the vairous config files to the home directory
-# run from the root directory with ~/config/setup.sh
+# run from the root directory with ~/dotfiles/setup.sh
 
 install_file() {
   if [ -e "$2" ] 
   then
-    echo "Moving $2 to ~/config/bak"
-    mv $2 config/bak
+    echo "Moving $2 to ~/dotfiles/bak"
+    mv $2 dotfiles/bak
   fi
   echo "Linking ~/$1 $2"
   ln -s $1 $2
 }
 
 # List of files to backup and link
-install_file config/etc/bash_profile .bash_profile
-install_file config/etc/bashrc .bashrc
-install_file config/etc/vim/gvimrc .gvimrc
-install_file config/etc/vim/vimrc .vimrc
-install_file config/etc/vim .vim
-mv dotfiles config
+install_file dotfiles/etc/bash_profile .bash_profile
+install_file dotfiles/etc/bashrc .bashrc
+install_file dotfiles/etc/vim/gvimrc .gvimrc
+install_file dotfiles/etc/vim/vimrc .vimrc
+install_file dotfiles/etc/vim .vim
 
